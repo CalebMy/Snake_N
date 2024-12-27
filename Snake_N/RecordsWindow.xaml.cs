8556364e95c0217b17b258dcb2246006695c167c
CalebMy <calebmyers899@gmail.com>
2024-12-27 16:16:34 +0700

    0.6.0
    ОНО РАБОТАЕТ!

--- Snake_N/RecordsWindow.xaml.cs
+++ Snake_N/RecordsWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Threading;
+using System.Xml.Linq;
 
 namespace Snake_N
 {
@@ -10,17 +11,18 @@ namespace Snake_N
     /// </summary>
     public partial class RecordsWindow : Window
     {
-        private readonly SnakeHighscore HighscoreList;
+        //private readonly SnakeHighscore HighscoreList;
         public RecordsWindow()
         {
             InitializeComponent();
             SnakeHighscore.LoadHighscoreList();
-            List<SnakeHighscore> records;
-            
-                //records.Add(new SnakeHighscore { PlayerName = playerName, Score = scores });
+            //List<SnakeHighscore> records;
+
+            //records.Add(new SnakeHighscore { PlayerName = playerName, Score = scores });
             //RecordsDataGrid.ItemsSource = records;
-        
-        
+            XElement ScoreList = XElement.Load("snake_highscorelist.xml");
+            RecordsDataGrid.DataContext = ScoreList;
+
         }
 
     }
