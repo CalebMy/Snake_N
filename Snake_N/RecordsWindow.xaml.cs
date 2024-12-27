1ea2b918a4487f37a10e099e57e4f6766cefdd18
CalebMy <calebmyers899@gmail.com>
2024-12-27 15:51:25 +0700

    0.5.0
    Спустя столько времени..

--- Snake_N/RecordsWindow.xaml.cs
+++ Snake_N/RecordsWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Threading;
 
@@ -14,38 +15,12 @@ namespace Snake_N
         {
             InitializeComponent();
             SnakeHighscore.LoadHighscoreList();
-            // Timer generating random BestLap double values from 1.0 to 4.0 every 5 seconds
-            DispatcherTimer randomlyUpdateDriverBestLapTimer = new DispatcherTimer();
-            randomlyUpdateDriverBestLapTimer.Interval = TimeSpan.FromSeconds(5);
-            randomlyUpdateDriverBestLapTimer.Tick += RandomlyUpdateDriverBestLapTimerOnTick;
-
-            SnakeHighscore = new SnakeHighscore();
-
-            Driver driver = new Driver { BestLap = 1.2, Name = "Meyer", StartNr = 1 };
-            driverViewModel.DriverList.Add(driver);
-
-            driver = new Driver { BestLap = 1.4, Name = "Sand", StartNr = 2 };
-            driverViewModel.HighscoreList.Add(driver);
-
-            driver = new Driver { BestLap = 1.5, Name = "Huntelaar", StartNr = 3 };
-            driverViewModel.HighscoreList.Add(driver);
-
-            this.DataContext = driverViewModel;
-
-            InitializeComponent();
-
-            randomlyUpdateDriverBestLapTimer.Start();
-        }
-        private void RandomlyUpdateDriverBestLapTimerOnTick(object sender, EventArgs eventArgs)
-        {
-            // Important to declare Random object not in the loop because it will generate the same random double for each driver
-            Random random = new Random();
-
-            foreach (var driver in driverViewModel.DriverList)
-            {
-                // Random double from 1.0 - 4.0 (Source code from https://stackoverflow.com/questions/1064901/random-number-between-2-double-numbers)
-                driver.BestLap = random.NextDouble() * (4.0 - 1.0) + 1.0;
-            }
+            List<SnakeHighscore> records;
+            
+                //records.Add(new SnakeHighscore { PlayerName = playerName, Score = scores });
+            //RecordsDataGrid.ItemsSource = records;
+        
+        
         }
 
     }
